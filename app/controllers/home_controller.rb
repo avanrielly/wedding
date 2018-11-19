de32fe5cc170fc81424aad7f9a4e64193958cecb
Alex O'Rielly <aorielly@truecar.com>
2018-11-18 19:31:50 -0700

    Styled the homepage and added other pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
   end
+
+  def view
+    if request.path == '/story'
+      render 'story'
+    elsif request.path == '/altitude'
+      render 'altitude'
+    elsif request.path == '/wedding'
+      render 'wedding'
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get :story, to: 'home#view'
+  get :altitude, to: 'home#view'
+  get :wedding, to: 'home#view'
+
   root "home#index"
 end
